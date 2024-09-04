d8a924019253cc2da120ff3a8efc077e3eb37198
waynetheisinger <wayne@mowdirect.co.uk>
2024-09-04 11:50:37 +0100

    feature: Add the archive

---
+++ src/ItJonction/BlackfireLabeler/task_blackfireArchiver.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace ItJonction\BlackfireLabeler;
+
+require_once __DIR__.'/commonTaskRunner.php';
+
+runBlackfireTask(function ($blackfireLabeler) {
+  $blackfireLabeler->archiveLoggedRequests(sys_get_temp_dir().'/blackfire_' . date('Y-m-d') . '.log');
+});
