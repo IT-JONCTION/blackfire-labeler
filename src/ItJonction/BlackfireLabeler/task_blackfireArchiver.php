c012a49574a6d8fed000ba6c2e24d3bc0bef1266
waynetheisinger <wayne@mowdirect.co.uk>
2024-09-04 14:16:42 +0100

    typo

--- src/ItJonction/BlackfireLabeler/task_blackfireArchiver.php
+++ src/ItJonction/BlackfireLabeler/task_blackfireArchiver.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace ItJonction\BlackfireLabeler;
 
-require_once __DIR__.'/commonTaskRunner.php';
+require_once __DIR__.'/CommonTaskRunner.php';
 
 runBlackfireTask(function ($blackfireLabeler) {
   $blackfireLabeler->archiveLoggedRequests(sys_get_temp_dir().'/blackfire_' . date('Y-m-d') . '.log');
